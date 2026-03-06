434c611311c2579b92ac1f8a472e51f694519dab
favoriiklim <icoskunsel@gmail.com>
2026-03-06 17:16:38 +0300

    0.1.2

---
+++ Models/MapProp.cs
@@ -0,0 +1,15 @@
+﻿namespace DMScreen.Client.Models
+{
+    public class MapProp
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty; 
+        
+        public double X { get; set; }
+        public double Y { get; set; }
+        
+        // This is the foundation for the future DM/Player vision split
+        public bool IsHidden { get; set; } = false;
+    }
+}
