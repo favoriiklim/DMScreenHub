ba7813b2bb73737c2f1848f52fe9bf791ce8ed40
favoriiklim <icoskunsel@gmail.com>
2026-03-06 10:26:58 +0300

    0.1.0

---
+++ Models/Wall.cs
@@ -0,0 +1,11 @@
+﻿namespace DMScreen.Client.Models
+{
+    public class Wall
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public double X1 { get; set; }
+        public double Y1 { get; set; }
+        public double X2 { get; set; }
+        public double Y2 { get; set; }
+    }
+}
